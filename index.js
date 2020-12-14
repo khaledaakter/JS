9ad999d1d0faa1c8f74a2c7c67c22314cfef1ef3
Khaleda Nahar <khaledanahar43@gmail.com>
2020-12-14 22:13:52 +0600

    this is first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+// link: https://www.youtube.com/watch?v=5wgH30Bvb3U
+/* ============= alert box ================
+ write alert box.
+ placeholder in alert box.
+ placeholder text with lable.
+ placeholder text with lable(2 time).
+
+ ============== like normal text ==================
+ write innerhtml in a id.
+ creat 2 variable.
+ (+ - * /) the all variables. 
+ create string variable.
+ what is this variable (number, string, ect)
+ what is the length of varible.
+
+ ====================== up to string =========================
+ (upper, lower) case of a word.
+ show index wise.
+ take more then one string varible (+ - * /) the all string variables. 
+
+*/
+
+// ================= task for today ================
+
+// javascript string function
+
+var x = "khaleda"
+document.write(x.charCodeAt(3))
+/*
+
+alert("Are You Sure");
+window.alert("Continue");
+
+document.write(prompt("Please Enter Your Name"));
+var name = prompt("Enter Your Name");
+document.write(prompt("Name please", "My name"), "<br>");
+document.write(name);
+
+confirm("Your info is correct?");
+window.confirm("sure??");
+document.write(window.confirm("Are you sure"));
+
+document.getElementById("header").innerHTML = "This is my page Header";
+
+var a = 10.9;
+var b = 22;
+document.write("a + b = ", (a + b));
+document.write("<br />")
+document.write("a - b = ", (a - b), "<br>");
+document.write("a * b = ", (a * b), "<br>");
+document.write("a / b = ", (a / b), "<br>");
+document.write("a % b = ", (a % b), "<br>");
+
+var x = "khaleda";
+document.write(typeof x, "<br>");
+document.write(typeof a, "<br>")
+document.write(x.length)
+
+document.write(x.toUpperCase(), "<br>");
+document.write(x.toLowerCase());
+document.write(x.toCappitalize())
+
+
+let roll = 324529;
+document.write(typeof let);
+*/
+
+// alert("Hey khaleda, Please enter your name");
+// document.prompt("Enter your name");
+
