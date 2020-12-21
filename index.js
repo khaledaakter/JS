2dfcad1d88e3fb1e5db6b66319374e982e3767e6
Khaleda Akter <khaledanahar43@gmail.com>
2020-12-21 17:55:33 +0600

    todes work

--- index.js
+++ index.js
@@ -5,13 +5,15 @@
  placeholder text with lable.
  placeholder text with lable(2 time).
  return placeholder text.
+ write somthing in a div. 
+ return placeholder text in a div.
 
  ============== like normal text ==================
  write innerhtml in a id.
  creat 2 variable.
  (+ - * /) the all variables. 
  create string variable.
- what is this variable (number, string, ect)
+ what is this variable (number, string, ect)//
  what is the length of varible.
  (upper, lower) case of a word. 
 
@@ -21,54 +23,13 @@
 // ================= javascript string function ================
 
 // show index wise.
-//  take more then one string varible (+ - * /) the all string variables.
+// What is the unic code of a index/value
+// take more then one string varible (+ - * /) the all string variables.
 // chack fist/last word/letter
 // 
 
-var a = 20;
-var b = 50;
-var c = "Khaleda"
-var d = "Akter"
-document.write(String.fromCharCode(100))
+var x = "Khaleda"
+var y = "Aziz"
+document.write(x.firstLetter("k"))
 
-/*
-
-alert("Are You Sure");
-window.alert("Continue");
-
-document.write(prompt("Please Enter Your Name"));
-var name = prompt("Enter Your Name");
-document.write(prompt("Name please", "My name"), "<br>");
-document.write(name);
-
-confirm("Your info is correct?");
-window.confirm("sure??");
-document.write(window.confirm("Are you sure"));
-
-document.getElementById("header").innerHTML = "This is my page Header";
-
-var a = 10.9;
-var b = 22;
-document.write("a + b = ", (a + b));
-document.write("<br />")
-document.write("a - b = ", (a - b), "<br>");
-document.write("a * b = ", (a * b), "<br>");
-document.write("a / b = ", (a / b), "<br>");
-document.write("a % b = ", (a % b), "<br>");
-
-var x = "khaleda";
-document.write(typeof x, "<br>");
-document.write(typeof a, "<br>")
-document.write(x.length)
-
-document.write(x.toUpperCase(), "<br>");
-document.write(x.toLowerCase());
-
-
-let roll = 324529;
-document.write(typeof let);
-*/
-
-// alert("Hey khaleda, Please enter your name");
-// document.prompt("Enter your name");
 
