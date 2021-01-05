90d18bd8a763256170fc11fb2aa4cd436450712b
Khaleda Akter <khaledanahar43@gmail.com>
2021-01-05 17:34:36 +0600

    all

--- index.js
+++ index.js
@@ -26,10 +26,24 @@
 // What is the unic code of a index/value
 // take more then one string varible (+ - * /) the all string variables.
 // chack fist/last word/letter
+// search fixed word print (index/value) wayes
+// search all same word 
+// change any word another word
+// cut some word
+// cut some word with fixed with
+// cut end of all txt from a word
 // 
-var x = prompt("your name")
-var y = "akter"
-confirm("sure")
-document.getElementById("header").innerHTML = (typeof x)
+
+
+
+// var x = prompt("your name")
+var x = "iLorem Ipsum is simply is dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+var y = "khalado"
+var a = 40
+var b = 50
+
+document.getElementById("header").innerHTML = (x.endsWith("k"))
+
+
 
 
