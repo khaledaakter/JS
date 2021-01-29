69f1fb2b994cf3c6f3a73bb10fa0a91c635f1b61
Khaleda Nahar <khaledanahar43@gmail.com>
2021-01-29 19:31:30 +0600

    add pdf

---
+++ js/index.js
@@ -0,0 +1,70 @@
+// link: https://www.youtube.com/watch?v=5wgH30Bvb3U
+
+//https://www.youtube.com/watch?v=EnoAlM1pEhc&list=PL6f7IPPkpDKn0iI2Y5dnEgyrEqw2VvJ2P&index=6
+
+/* ============= alert box ================
+ write alert box.
+ placeholder in alert box.
+ placeholder text with lable.
+ placeholder text with lable(2 time).
+ return placeholder text.
+ write somthing in a div. 
+ return placeholder text/innerhtml in a div.
+
+*/
+
+/*
+ ============== like normal text ==================
+ creat 2 variable.
+ (+ - * /) the all variables.
+ create string/object variable.
+ what is this variable (number, string, ect)
+ what is the length of varible.
+*/
+// var a = "khaleda";
+// var b = {"name":"khaleda","age":"50"}
+// document.write(b[1])
+/*
+============================================
+print object value/object value add
+
+================= javascript string function/mathod ================
+(upper, lower) case of a word. 
+show index wise.
+show index (by charecter)
+What is the unic code of a index/value
+take more then one string varible (+ - * /) the all string variables.
+chack fist/last word/letter
+
+search fixed word print (index/value) wayes
+search all same word 
+change any word another word
+cut some word/some word with fixed with
+cut end of all txt from a word
+*/
+
+<<<<<<< HEAD:index.js
+var y = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+=======
+
+// ================= javascript string function ================
+
+// show index wise.
+// What is the unic code of a index/value
+// take more then one string varible (+ - * /) the all string variables.
+// chack fist/last word/letter
+// search fixed word print (index/value) wayes
+// search all same word 
+// change any word another word
+// cut some word
+// cut some word with fixed with
+// cut end of all txt from a word
+// 
+
+// var a = prompt("name");
+var x = 45;
+var y = 50;
+var b = "Although not not strictly associated with the MVVM pattern, Vue’s design was partly inspired by it. As a convention, we often use the variable vm (short for ViewModel) to refer to our Vue instance."
+document.getElementById("header").innerHTML = (b.replace(/"not","am"/g))
+>>>>>>> df43f987cd2c1148985a1822f9227b835c493f7f:js/index.js
