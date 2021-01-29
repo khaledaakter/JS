c2062ffcffaaf49a29a7ae09a756d3e9cb5ba97d
Khaleda Nahar <khaledanahar43@gmail.com>
2021-01-29 19:26:26 +0600

    add pdf

--- index.js
+++ index.js
@@ -6,44 +6,39 @@
  placeholder text with lable(2 time).
  return placeholder text.
  write somthing in a div. 
- return placeholder text in a div.
-
- ============== like normal text ==================
- write innerhtml in a id.
- creat 2 variable.
- (+ - * /) the all variables. 
- create string variable.
- what is this variable (number, string, ect)//
- what is the length of varible.
- (upper, lower) case of a word. 
+ return placeholder text/innerhtml in a div.
 
 */
 
+/*
+ ============== like normal text ==================
+ creat 2 variable.
+ (+ - * /) the all variables.
+ create string/object variable.
+ what is this variable (number, string, ect)
+ what is the length of varible.
+*/
+// var a = "khaleda";
+// var b = {"name":"khaleda","age":"50"}
+// document.write(b[1])
+/*
+============================================
+print object value/object value add
 
-// ================= javascript string function ================
+================= javascript string function/mathod ================
+(upper, lower) case of a word. 
+show index wise.
+show index (by charecter)
+What is the unic code of a index/value
+take more then one string varible (+ - * /) the all string variables.
+chack fist/last word/letter
 
-// show index wise.
-// What is the unic code of a index/value
-// take more then one string varible (+ - * /) the all string variables.
-// chack fist/last word/letter
-// search fixed word print (index/value) wayes
-// search all same word 
-// change any word another word
-// cut some word
-// cut some word with fixed with
-// cut end of all txt from a word
-// 
+search fixed word print (index/value) wayes
+search all same word 
+change any word another word
+cut some word/some word with fixed with
+cut end of all txt from a word
+*/
 
-
-
-// var x = prompt("your name")
-var x = "iLorem Ipsum is simply is dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
-var y = "khalado"
-var a = 40
-var b = 50
-
-document.getElementById("header").innerHTML = (x.endsWith("k"))
-
-
-
+var y = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
 
